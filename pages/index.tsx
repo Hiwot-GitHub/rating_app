6b8bd04b8179d8e41e1a6a4fafd2238639863626
Sisay Hiwot <angelhiwot@gmail.com>
2022-11-26 11:16:28 +0200

    version 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,44 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import SmallStar from './components/SmallStar'
+import ThankYou from './components/ThankYou'
 const Home: NextPage = () => {
+  function clickHandler () {
+    return <div><ThankYou /></div>
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex w-full flex-1 flex-col items-center justify-center  bg-VeryDarkBlue">
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+          <div className="flex-col flex-wrap w-96 shodow rounded-2xl  bg-DarkBlue">
+           
+            <div className='mt-4 ml-5'>
+              <button className='bg-MediumGray py-2 px-2 rounded-full'><SmallStar/></button></div>
+            <h1 className="font-bold text-White mt-6 ml-5 align-self-left ">How did we do?</h1>
+            <p className="font-overpass text-LightGray 15px mt-4 ml-5 mr-2 left-0">
+              Please let us know how we did with your support
+              request. All feedback is appreciated to help us improve our offering!
+               
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <div className="flex-col ">
+              <div className="flex ml-5 mr-10 mt-4 mb-4 justify-between">
+                
+                <button className="bg-MediumGray focus:bg-White hover:bg-LightGray text-LightGray border-solid-VeryDarkBlue py-2 px-4 rounded-full">1</button>
+                <button className="bg-MediumGray focus:bg-White hover:bg-LightGray text-LightGray  py-2 px-4 rounded-full ">2</button>
+                <button className="bg-MediumGray focus:bg-White hover:bg-LightGray text-LightGray  py-2 px-4 rounded-full">3</button>
+                <button className="bg-MediumGray focus:bg-White hover:bg-LightGray  text-LightGray font-weight-700 py-2 px-4 rounded-full">4</button>
+                <button className="bg-MediumGray focus:bg-White  hover:bg-LightGray text-LightGray py-2 px-4 rounded-full ">5</button>
+              </div>
+              <button className="bg-Orange hover:bg-LightOrange  ml-5 mb-4  py-1 px-32 rounded-3xl text-White onClick={() => {return <div><ThankYou /></div> }}">SUBMIT</button>
+            </div>
+          </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors:{
+      transparent: 'transparent',
+      'Orange': '#fb7413',
+      'White': '#ffffff',
+      'LightGray': '	#959eac',
+      'MediumGray': '#7c8798',
+      'DarkBlue': '#252d37',
+      'VeryDarkBlue' : '#121417',
+      'LightOrange': '	#fc9145'
+      
+    },
+    screens:{
+      'sm': '375px',
+      'lg': '1440px'
+    },
     extend: {},
   },
   plugins: [],
